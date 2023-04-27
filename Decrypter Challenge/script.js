9349da2c93f6e8bf86c4e0fb4ce74df35a8eb928
Bryan Alonso <balonsodev@gmail.com>
2023-04-27 13:13:09 -0600

    se incluye la formula de encriptacion

--- Decrypter Challenge/script.js
+++ Decrypter Challenge/script.js
@@ -1,6 +1,15 @@
-var palabra = "casa";
+var output = " ";
+var input = "la casa de mi mama";
 
-function encriptar(palabra) {
-  palabra.replaceAll("a", "ai");
-  console.log(palabra);
+function encript() {
+  //let input = document.getElementById(inputText.value);
+
+  let inputDecoded = input
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "over")
+    .replace(/u/gi, "ufat");
+  console.log(inputDecoded);
 }
+encript(input);
