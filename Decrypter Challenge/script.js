dfde716ca363cfb29520342a3782f5eea311173e
Bryan Alonso <balonsodev@gmail.com>
2023-04-27 13:20:41 -0600

    se incluye la formula para desencriptar

--- Decrypter Challenge/script.js
+++ Decrypter Challenge/script.js
@@ -1,15 +1,29 @@
 var output = " ";
 var input = "la casa de mi mama";
+var inputCoded = "lai caisai denter mimes maimai";
 
-function encript() {
+function encrypt() {
   //let input = document.getElementById(inputText.value);
 
-  let inputDecoded = input
+  let inputToCode = input
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
     .replace(/a/gi, "ai")
     .replace(/o/gi, "over")
     .replace(/u/gi, "ufat");
-  console.log(inputDecoded);
+  console.log(inputToCode);
 }
-encript(input);
+
+function decrypt() {
+  //let input = document.getElementById(inputText.value);
+
+  let inputToDecode = inputCoded
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/over/gi, "o")
+    .replace(/ufat/gi, "u");
+  console.log(inputToDecode);
+}
+encrypt(input);
+decrypt(inputCoded);
