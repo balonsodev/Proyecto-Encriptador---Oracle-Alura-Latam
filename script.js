c21c44d24791e2836a61882723142dc6e69e39dd
Bryan Alonso <balonsodev@gmail.com>
2023-05-10 16:44:07 -0600

    Encriptador funcional

---
+++ script.js
@@ -0,0 +1,29 @@
+//var output = " ";
+const textoPrueba = "la casa";
+
+function encrypt() {
+  var textoI = document.getElementById("inputText").value;
+  let inputToCode = textoI
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+  console.log(inputToCode);
+  document.getElementById("textoEncriptado").innerHTML = inputToCode;
+}
+
+function decrypt() {
+  let input = document.getElementById("inputText").value;
+
+  let inputToDecode = input
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+  console.log(inputToDecode);
+  document.getElementById("outputText").innerHTML = inputToDecode;
+}
+
+//decrypt(inputCoded);
