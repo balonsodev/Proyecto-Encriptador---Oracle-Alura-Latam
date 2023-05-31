ec1e9df260cbf9e39f34d663417e1abf5465630c
Bryan Alonso <balonsodev@gmail.com>
2023-05-30 22:33:50 -0600

    modificaciones al script.js, se agrega validacion,esconder dibujo, y se mejra el fondo ver1.2

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-function copyToClipboard() {
-  var resultField = document.getElementById("outputText");
-  resultField.select();
-  document.execCommand("copy");
-  alert("El texto ha sido incluido al portapapeles");
-  resultField = "";
+const inputText = document.getElementById("inputText");
+const outputText = document.getElementById("outputText");
+const btncopiar = document.querySelector(".btnCopiar");
+document.getElementById("btn-copy").style.display = "none";
+
+function btnEncriptar() {
+  console.log("btnEncriptar ejecutado");
+  let encriptValue = document.getElementById("inputText").value;
+
+  if (!validarTexto()) {
+    const textoEncriptado = encrypt(encriptValue);
+    document.getElementById("outputText").style.backgroundImage = "none";
+    document.getElementById("btn-copy").style.display = "block";
+  } else {
+    console.log("no valida");
+    return false;
+  }
 }
 
-function encrypt() {
-  var textoI = document.getElementById("inputText").value;
-  textoI = textoI.toLowerCase();
-  let inputToCode = textoI
+function validarTexto() {
+  let textoEscrito = document.getElementById("inputText").value;
+  let regExp = /[0-9a-zA-ZñÑáéíóúÁÉÍÓÚ@#$!-`~%&]/gm;
+  let validador = textoEscrito.match(regExp);
+  if (!validador || validador === 0) {
+    alert("Solo son permitidas letras minúsculas y sin acentos");
+    location.reload();
+    return true;
+  }
+}
+
+function encrypt(inputTextA) {
+  inputTextA = document.getElementById("inputText").value;
+  inputTextA = inputTextA.toLowerCase();
+  inputToCode = inputTextA
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
     .replace(/a/gi, "ai")
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
   resultado(inputToCode);
-  //document.getElementById("outputText").innerHTML = inputToCode;
-  textoI = "";
-  inputToCode = "";
+  inputToCode = " ";
 }
+
 function resultado(result) {
   document.getElementById("outputText").innerHTML = result;
 }
@@ -34,7 +55,14 @@ function decrypt() {
     .replace(/ufat/g, "u");
   console.log(inputToDecode);
   resultado(inputToDecode);
-  //document.getElementById("outputText").innerHTML = inputToDecode;
   input = "";
   inputToDecode = "";
+}
+
+function copyToClipboard() {
+  var resultField = document.getElementById("outputText");
+  resultField.select();
+  document.execCommand("copy");
+  alert("El texto ha sido incluido al portapapeles");
+  resultField = "";
 }
